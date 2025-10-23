5228df3fd04feea72b6ca0676707795446d9bf8d
Niklas E. Mork <niklas.engjom.mork@vlfk.no>
2025-10-23 16:40:14 +0200

    Password generator project

---
+++ Solo Project 3 Passordgenerator/index.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let pass1EL = document.getElementById('pass1-el')
+let pass2EL = document.getElementById('pass2-el')
+
+function generator() {
+    let password1 = ""
+    let password2 = ""
+    
+    for (i = 0; i < 15; i++) {
+        password1 += characters[Math.floor(Math.random() * characters.length)]
+        password2 += characters[Math.floor(Math.random() * characters.length)]
+    }
+    pass1EL.textContent = password1
+    pass2EL.textContent = password2
+}
+
+
+
+
